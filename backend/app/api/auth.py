48ea6b96a7bd5eacc05691b72c41c32405cf1a1f
sushmabhagwan <sushmabhagwan@icloud.com>
2026-06-02 13:18:45 -0500

    Add user registration API with password hashing

---
+++ backend/app/api/auth.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
+
+from app.schemas.user import UserCreate
+from app.models.user import User
+from app.database.session import get_db
+from app.utils.security import hash_password
+
+router = APIRouter()
+
+
+@router.post("/register")
+def register(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User registered successfully"
+    }
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI
 from app.database.database import Base, engine
 from app.models import User
 
+from app.api.auth import router as auth_router
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="AI Mock Interview Platform",
     version="1.0.0"
 )
+
+app.include_router(auth_router)
 
 
 @app.get("/")
