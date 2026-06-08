e777fd1ee59f4c503218301f753ae314c8ad393a
sushmabhagwan <sushmabhagwan@icloud.com>
2026-06-08 14:56:15 -0500

    Update CORS for deployment1

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,9 +18,10 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ],
+    #allow_origins=[
+       # "http://localhost:5173"
+   # ],
+   allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
